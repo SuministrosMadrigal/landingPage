8d8b5acd4f4c25b00243b37d4ec58098f0ea903d
pedroduranc1 <61203515+pedroduranc1@users.noreply.github.com>
2024-04-25 12:05:10 -0400

    UPDATE DE LIBRERIA PDF

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,29 @@
 import './App.css'
 import BROCHURE from "../public/brochure.pdf";
 
+import { Worker, Viewer } from '@react-pdf-viewer/core';
+import '@react-pdf-viewer/core/lib/styles/index.css';
 
 function App() {
   const PDFFILE = `${BROCHURE}#toolbar=0&navpanes=0&scrollbar=0`
 
   return (
     <>
-      <div className='bg-black w-full min-h-screen'>
+      {/* <div className='bg-black w-full min-h-screen'>
         <iframe
           className='h-screen'
           src={PDFFILE}
           width="100%"
           style={{ border: 'none' }}
         />
-      </div>
+      </div> */}
+
+      <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
+        <div className='w-full min-h-screen'>
+          <Viewer fileUrl={PDFFILE} />
+
+        </div>
+      </Worker>
     </>
   )
 }
